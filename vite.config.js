324111561a8c6a731a0f7978aca1e152649574ee
fifakung <fifakung24@gmail.com>
2025-08-05 09:53:59 +0700

    fixde

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server:{
-    port: 3001
+  base: '/', // 👈 สำคัญ
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3001,
   },
 })
